8c3e243c9bd6f572b6ca7369eb8576b2ae17da12
Fadly Tanjung <fadlysyah96@gmail.com>
2020-05-10 23:25:27 +0700

    Update 8 Sites

--- src/app.py
+++ src/app.py
@@ -54,6 +54,6 @@ def crawlprocess():
 
         
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
-    # port = int(os.environ.get("PORT",5000))
-    # app.run(host="0.0.0.0",port=port)
+    # app.run(debug=True, port=8080)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
